f3a34d7bc775598e93934f8cb45e8bc297602b1c
ali_a <ali_a@ALIAYTEKIN>
2024-11-01 14:45:21 +0300

    Add project files.

---
+++ Context/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace NetWithElasticSearch.Context
+{
+    public sealed class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("");
+        }
+
+        public DbSet<Travel> Travels { get; set; }
+    }
+}
+
+public sealed class Travel
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+}
